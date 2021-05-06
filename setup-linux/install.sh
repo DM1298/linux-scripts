862186b42159d0b115240048b0033c1e883448ea
David Martos <david.martos9.7@gmail.com>
2021-05-06 13:13:56 +0200

    Canvis a cometre: 	fitxer nou:       setup-linux/install.sh 	fitxer nou:       setup-linux/tunea.sh
     Canvis no «staged» per a cometre:
    	suprimit:         actualiza_zsh.sh
    	suprimit:         setup-atom.sh
    	suprimit:         setup.sh
    	suprimit:         setup.sh.save

---
+++ setup-linux/install.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+apt update
+apt upgrade -y
+
+apt install -y zsh filezilla remmina nmap wireshark git
+
+zsh
+
+# OH MY ZSH
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# VS Code
+# Add repo and install key
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+apt install apt-transport-https
+apt update
+apt install code
+
+apt autoclean
+apt autoremove
---
+++ setup-linux/tunea.sh
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/DM1298/dotfiles/master/zshrc
+mv zshrc ~/.zshrc
+rm zshrc
